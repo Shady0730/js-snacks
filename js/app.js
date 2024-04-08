a600ea235e1ca1b03786a3157f5000d09e65f0a5
Simone0730 <simone0730@hotmail.com>
2024-04-08 11:30:46 +0200

    js snacks

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log('JS-SNACK1')
+
+const numA = parseInt(prompt ('Inserisci un numero')); //numero
+console.log (numA)
+
+const numB = parseInt(prompt ('Inserisci un altro numero')); //numero
+console.log (numB);
+
+if(numA > numB){
+    console.log(numA , 'é maggiore di ', numB,);
+}
+
+else{
+    console.log(numB , 'é maggiore di ', numA,);
+}
+
+
+
+
